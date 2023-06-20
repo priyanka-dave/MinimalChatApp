6980d228a814801572b16cf1e84d8b2d5ee58911
priyanka-dave <priyankadave@promactinfo.com>
2023-06-20 19:24:09 +0530

    "Initial commits"

---
+++ MinimalChatApp/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinimalChatApp.Models
+{
+    public class Message
+    {
+        [Required]
+        public int MessageId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User SenderId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime TimeStamp { get; set; }= DateTime.Now;
+    }
+}
---
+++ MinimalChatApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalChatApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+    }
+}
